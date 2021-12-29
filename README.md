027ff744070696d391b1d097497cfdf890169d99
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2021-12-29 23:09:38 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Words with letters
+
+This work is devoted to God.
+
+Can be used for "Microsoft Ultimate Word Games - Word Twister".
+
+## References
+
+The word list is based on [Free German Dictionary](https://sourceforge.net/projects/germandict/) by Jan Schreiber, 
+which has been put into the public domain.
+
+
+## Scripts
+
+The script `post_process_words.js` converts the word list `words.txt` to the JavaScript file `words.js`.
---
+++ index.js
@@ -0,0 +1,91 @@
+const WORDS = 'words'
+
+export function generateSearchTree(words) {
+  const tree = new Map()
+  for (const word of words) {
+    const word2 = word.toLowerCase()
+      .replaceAll('ä', 'a')
+      .replaceAll('ö', 'o')
+      .replaceAll('ü', 'u')
+      .replaceAll('ß', 'ss')
+    const characters = word2.split('')
+    characters.sort()
+    let subTree = tree
+    for (const character of characters) {
+      if (subTree.has(character)) {
+        subTree = subTree.get(character)
+      } else {
+        const previousSubtree = subTree
+        subTree = new Map()
+        previousSubtree.set(character, subTree)
+      }
+    }
+    let wordsWithCharacters
+    if (subTree.has(WORDS)) {
+      wordsWithCharacters = subTree.get(WORDS)
+    } else {
+      wordsWithCharacters = new Set()
+      subTree.set(WORDS, wordsWithCharacters)
+    }
+    wordsWithCharacters.add(word)
+  }
+  return tree
+}
+
+export function findWordsWithCharacters(searchTree, characters) {
+  characters = characters.map(character => character.toLowerCase())
+  characters.sort()
+
+  let subTree = searchTree
+  for (const character of characters) {
+    if (subTree.has(character)) {
+      subTree = subTree.get(character)
+    } else {
+      return []
+    }
+  }
+  const words = subTree.has(WORDS) ? Array.from(subTree.get(WORDS)) : []
+  return words
+}
+
+export function findWordsThatCanBeMadeOutOfCharacters(searchTree, characters) {
+  characters = characters.map(character => character.toLowerCase())
+  const combinations = Array.from(generateCombinations(characters)).filter(sequence => sequence.length >= 3)
+  const words = unique(flatten(combinations.map(characters => findWordsWithCharacters(searchTree, Array.from(characters)))))
+  return words
+}
+
+function flatten(arrays) {
+  return [].concat(...arrays)
+}
+
+function unique(values) {
+  return Array.from(new Set(values))
+}
+
+export function generateCombinations(values) {
+  const array = [...values];
+  array.sort();
+  const indexSubSequences = getIndexSubSequences(array.length);
+  const subSequences = indexSubSequences.map((indexSubSequence) => indexSubSequence.map((index) => array[index]));
+  const subSets = subSequences.map((subSequence) => subSequence);
+  const result = subSets;
+  return result;
+}
+
+function getIndexSubSequences(length) {
+  let subSequences = [[]];
+  const result = [...subSequences];
+  do {
+    const nextSubSequences = [];
+    for (const subSequence of subSequences) {
+      const lastValue = subSequence.length > 0 ? subSequence[subSequence.length - 1] : -1;
+      for (let value = lastValue + 1; value < length; value++) {
+        nextSubSequences.push([...subSequence, value]);
+      }
+    }
+    result.push(...nextSubSequences);
+    subSequences = nextSubSequences;
+  } while (subSequences.length > 0);
+  return result;
+}
